17f5e5234ace27c098533150134892fc5586bc10
Ari Maulana Hardan <111731051+arimaulanahardan@users.noreply.github.com>
2024-12-30 10:55:52 +0700

    STYLE: change margin for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Dashboard() {
   return (
-    <div className="w-full flex flex-col justify-center mt-10 text-center items-center">
+    <div className="w-full flex flex-col justify-center mt-40 text-center items-center">
         <h1>Dashboard</h1>
         <Button onClick={logout}>Logout</Button>
     </div>
